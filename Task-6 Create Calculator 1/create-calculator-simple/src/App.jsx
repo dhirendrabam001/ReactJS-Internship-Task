4a5d171a01a84b73e355922d54c4ea8b97bf1a1e
dhirendrabam001 <dhirendrabam12345@gmail.com>
2025-09-03 23:15:46 -0700

    updated code

--- Task-6 Create Calculator 1/create-calculator-simple/src/App.jsx
+++ Task-6 Create Calculator 1/create-calculator-simple/src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
 import styles from "./App.module.css";
+import ButtonContainer from "./components/ButtonContainer";
+import Display from "./components/Display";
 
 function App() {
   return (
     <div className={styles.container}>
-      <input className={styles.display} type="text" />
-      <div className={styles.buttonContainer}>
-        <button className={styles.buttons}>C</button>
-        <button className={styles.buttons}>1</button>
-        <button className={styles.buttons}>2</button>
-        <button className={styles.buttons}>+</button>
-      </div>
+      <Display />
+      <ButtonContainer />
     </div>
   );
 }
